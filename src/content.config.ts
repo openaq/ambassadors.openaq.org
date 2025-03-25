169e90841cb06e5dbdac611b536a68aba3d388f2
Russ Biggs <russbiggs@gmail.com>
2025-03-25 15:18:14 -0600

    initial

---
+++ src/content.config.ts
@@ -0,0 +1,47 @@
+import { defineCollection, z } from "astro:content";
+
+import { glob } from "astro/loaders";
+import { ISO_A2 } from './utils/constants'
+
+const [isoCode, ...isoCodes] =Object.keys(ISO_A2)
+
+const ambassadors = defineCollection({
+  loader: glob({ pattern: ["*.md"], base: "src/content/people/ambassadors" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      country_iso: z.enum([isoCode, ...isoCodes]),
+      year: z.number().refine((x) => x > 2020 && x < 2030, {
+        message: "Must be a valid year",
+      }),
+      timestamp:  z.string().time(),
+      presentation: z.string(),
+      email: z.string().email().optional(),
+      image: image(),
+      bluesky: z.string().url().optional(),
+      linkedin: z.string().url().optional(),
+      github: z.string().url().optional(),
+      x: z.string().url().optional(),
+      mastadon: z.string().url().optional(),
+      orcid: z.string().url().optional(),
+      researchGate: z.string().url().optional(),
+      googleScholar: z.string().url().optional(),
+    }),
+});
+
+
+const cohorts = defineCollection({
+    loader: glob({ pattern: ["*.md"], base: "src/content/people/cohorts" }),
+    schema: () =>
+      z.object({
+        year: z.number().refine((x) => x > 2020 && x < 2030, {
+          message: "Must be a valid year",
+        }),
+        youtube_url: z.string().url(),
+      }),
+  });
+
+export const collections = {
+    ambassadors,
+    cohorts
+};
---
+++ src/content/people/ambassadors/alejandro.md
@@ -0,0 +1,10 @@
+---
+name: "Alejandro Piracoca"
+image: "@assets/images/maiha.png"
+linkedin: "https://www.linkedin.com/in/aminathmaiha/"
+country_iso: "CO"
+year: 2025
+timestamp: "00:02:22"
+presentation: "public/foobar.pdf"
+---
+Alejandro Piracoca, M.Sc., is an environmental engineer specializing in air quality, environmental modeling and environmental data analysis. With expertise in emissions modeling and development of air pollution reduction strategies, he has collaborated on projects with multilateral and national organizations to advance environmental solutions. As a researcher at Los Andes University and a professional at Hill Consulting, he has contributed to air pollution monitoring, modeling and mitigation, emissions modeling and development of inputs for use in public policy. Alejandro holds a master's degree from Los Andes University and is proficient in data analysis, GIS, and programming for environmental applications. His work integrates science and policy to drive impactful sustainability initiatives.
