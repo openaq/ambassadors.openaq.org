54ad598edd90c2d752c790ab9e31937b9b84a526
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2025-04-10 09:33:37 +0200

    add purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "never",
+  },
   output: "static",
   experimental: {
     svg: true,
     responsiveImages: true,
   },
+  integrations: [purgecss()],
 
   markdown: {
     remarkPlugins: [remarkModifiedTime],
   },
-  site: 'https://openaq.github.io',
-  base: 'ambassadors.openaq.org',
+  site: "https://openaq.github.io",
+  base: "ambassadors.openaq.org",
 });
